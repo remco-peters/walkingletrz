a9c36d1d15057e6d25eadabae7bd522e27bcf502
Remco Peters <remco_peters@outlook.com>
2019-03-15 13:53:39 +0100

    generate starting letters and added lettermanager

---
+++ Walking Letrz/Assets/Scripts/LetterManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LetterManager : MonoBehaviour
+{
+    public StartingLetters StartingLettersClass;
+
+    private void Start()
+    {
+        Instantiate(StartingLettersClass);
+    }
+}
---
+++ Walking Letrz/Assets/Scripts/StartingLetters.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartingLetters : MonoBehaviour
+{
+    private string[] availableLetters = { "a", "b,", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+        "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "z" };
+    
+    public string firstLetter { get; set; }
+    public string secondLetter { get; set; }
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        firstLetter = availableLetters[Random.Range(0, availableLetters.Length)];
+        secondLetter = availableLetters[Random.Range(0, availableLetters.Length)];
+        Debug.Log(firstLetter + " " + secondLetter);
+    }
+}
