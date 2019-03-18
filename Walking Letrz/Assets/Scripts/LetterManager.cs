5d595e7e068dce40dfd0b044efcf587745291aa3
Wouter Buytenhuijs <571135@student.inholland.nl>
2019-03-18 14:21:21 +0100

    Lettervalues & calculatevalue

--- Walking Letrz/Assets/Scripts/LetterManager.cs
+++ Walking Letrz/Assets/Scripts/LetterManager.cs
@@ -1,15 +1,51 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 public class LetterManager : MonoBehaviour
 {
     public StartingLetters StartingLettersClass;
     public PlayerLetters PlayerLettersClass;
 
+    public char[] PlacedLetters{get;set;}
+
+    public Dictionary<string, object> charactersValues;
+
     private void Start()
     {
+        InitCharactersValues();
         Instantiate(StartingLettersClass);
         Instantiate(PlayerLettersClass);
+    }
+
+    public int CalculatePoints(string[] letters){
+        int value = 0;
+        foreach(string letter in letters)
+        {
+            value += (byte)charactersValues.First(x => x.Key == letter).Value;
+        }
+        return value;
+    }
+
+    public void InitCharactersValues(){
+ 
+        using (StreamReader r = new StreamReader("Assets\\Scripts\\settings.json"))
+        {
+            string json = r.ReadToEnd();
+            var items = (Dictionary<string, object>)MiniJSON.Json.Deserialize(json);
+            foreach (var item in items)
+            {
+                if (item.Key != "lettervalues") continue;
+                charactersValues = (Dictionary<string, object>)item.Value;
+            }
+        }
+    }
+
+    public bool CheckWord(string word){
+        //todo check if word is in list
+        return true;
     }
 }
