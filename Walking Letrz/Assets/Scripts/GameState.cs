1f41495b7aa9f7b67094e2e1f5087a3ac8ef15b5
Susanne <sjmprick@gmail.com>
2019-03-18 16:54:07 +0100

    Added two buttons

--- Walking Letrz/Assets/Scripts/GameState.cs
+++ Walking Letrz/Assets/Scripts/GameState.cs
@@ -10,6 +10,8 @@ public class GameState : MonoBehaviour
     public Camera CameraClass;
     public MyPlayer PlayerClass;
     public HUD HUDClass;
+    public RemoveWordBtn RemoveWordBtnClass;
+    public PlaceWordBtn PlaceWordBtnClass;
     
     // Start is called before the first frame update
     void Start()
@@ -17,9 +19,14 @@ public class GameState : MonoBehaviour
         Assert.IsNotNull(CameraClass, "Camera misses in GameState");
         Assert.IsNotNull(PlayerClass, "Player misses in GameState");
         Assert.IsNotNull(HUDClass, "HUD misses in GameState");
+        Assert.IsNotNull(RemoveWordBtnClass, "RemoveBtn misses in GameState");
+        Assert.IsNotNull(PlaceWordBtnClass, "PlaceBtn misses in GameState");
 
         Instantiate(CameraClass);
         MyPlayer Player = Instantiate(PlayerClass);
+        RemoveWordBtn RemoveBtn = Instantiate(RemoveWordBtnClass);
+        PlaceWordBtn PlaceBtn = Instantiate(PlaceWordBtnClass);
+
         HUD HUD = Instantiate(HUDClass);
         HUD.Player = Player;
     }
