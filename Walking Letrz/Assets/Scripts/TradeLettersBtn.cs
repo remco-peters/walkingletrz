b7483d9acc9039434901213b41058a9382eace2c
Wouter Buytenhuijs <571135@student.inholland.nl>
2019-03-22 12:09:05 +0100

    TradeLetterbtn

---
+++ Walking Letrz/Assets/Scripts/TradeLettersBtn.cs
@@ -0,0 +1,13 @@
+﻿using Assets.Scripts;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TradeLettersBtn : MyMonoBehaviour
+{
+    public event UnityAction OnTradeTouched;
+    public LetterManager LetterManager { get; set; }
+    private void OnMouseDown()
+    {
+        OnTradeTouched();
+    }     
+}
