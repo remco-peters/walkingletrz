c0fcc4b467cf63d816a4e74e9155ca879379fe5e
Remco Peters <remco_peters@outlook.com>
2019-04-18 14:02:39 +0200

    set display name and show it on player profile get leaderboards

--- Walking Letrz/Assets/Scripts/AccountManager.cs
+++ Walking Letrz/Assets/Scripts/AccountManager.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using PlayFab;
 using PlayFab.ClientModels;
+using PlayFab.PlayStreamModels;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AccountManager : MonoBehaviour
+
 {
+    public static PlayerProfileModel CurrentPlayer;
+    private GetLeaderboardRequest leaderboardRequest;
     private void Awake()
     {
         DontDestroyOnLoad(this);
@@ -14,22 +19,54 @@ public class AccountManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        var request = new LoginWithAndroidDeviceIDRequest();
-        request.OS = SystemInfo.operatingSystem;
-        request.TitleId = "F537C";
-        request.AndroidDevice = SystemInfo.deviceModel;
-        request.AndroidDeviceId = SystemInfo.deviceUniqueIdentifier;
-        request.CreateAccount = true;
-        PlayFabClientAPI.LoginWithAndroidDeviceID(request, Success, Error);
+        if (CurrentPlayer == null)
+        {
+            var request = new LoginWithAndroidDeviceIDRequest
+            {
+                OS = SystemInfo.operatingSystem,
+                TitleId = "F537C",
+                AndroidDevice = SystemInfo.deviceModel,
+                AndroidDeviceId = SystemInfo.deviceUniqueIdentifier,
+                CreateAccount = true
+            };
+            PlayFabClientAPI.LoginWithAndroidDeviceID(request, Success, OnFailure);
+            leaderboardRequest = new GetLeaderboardRequest {StatisticName = "Score", MaxResultsCount = 10};
+        }
+    }
+
+    private void LeaderboardSuccess(GetLeaderboardResult result)
+    {
+        //TODO: Show list in UI
+        Debug.Log(result.Leaderboard);
     }
 
     private void Success(LoginResult result)
     {
-        Debug.Log("Success");
+        var playerRequest = new GetPlayerProfileRequest();
+        playerRequest.PlayFabId = result.PlayFabId;
+        PlayFabClientAPI.GetPlayerProfile(playerRequest, PlayerProfileSuccess, OnFailure);
+        PlayFabClientAPI.GetLeaderboard(leaderboardRequest, LeaderboardSuccess, OnFailure);
+    }
+    
+    private void PlayerProfileSuccess(GetPlayerProfileResult result)
+    {
+        CurrentPlayer = result.PlayerProfile;
     }
 
-    private void Error(PlayFabError error)
+    private void OnFailure(PlayFabError error)
     {
-        Debug.Log("error");
+        Debug.Log(error.GenerateErrorReport());
+    }
+
+    //used from input field from editor on profile scene
+    public void SetDisplayName(string displayName)
+    {
+        var displayNameRequest = new UpdateUserTitleDisplayNameRequest {DisplayName = displayName};
+        PlayFabClientAPI.UpdateUserTitleDisplayName(displayNameRequest, DisplayNameSuccess, OnFailure);
+    }
+
+    private void DisplayNameSuccess(UpdateUserTitleDisplayNameResult result)
+    {
+        Debug.Log($"New display name: {result.DisplayName}");
     }
 }
---
+++ Walking Letrz/Assets/Scripts/ProfileInputField.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProfileInputField : MonoBehaviour
+{
+    public InputField InputField;
+    private void Awake()
+    {
+        if (AccountManager.CurrentPlayer.DisplayName.Length > 0)
+            InputField.text = AccountManager.CurrentPlayer.DisplayName;
+    }
+}
