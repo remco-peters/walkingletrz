6f496850b6a910ffebd5794a56fc8f2e1b5094f2
Susanne <sjmprick@gmail.com>
2019-04-29 22:30:13 +0200

    Edited menu

---
+++ Walking Letrz/Assets/MainMenuScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public Text Username;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Username.text = AccountManager.CurrentPlayer.DisplayName;
+    }
+}
---
+++ Walking Letrz/Assets/MenuBtnScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuBtnScript : MonoBehaviour
+{
+    public GameObject menuPanel;
+    public Sprite arrowLeft;
+    private Sprite hamburgerImg;
+
+    void Start()
+    {
+        hamburgerImg = GetComponent<Image>().sprite;
+    }
+
+    public void OnMenuClicked()
+    {
+        if (!menuPanel.activeInHierarchy)
+        {
+            menuPanel.SetActive(true);
+            GetComponent<Image>().sprite = arrowLeft;
+        } else
+        {
+            menuPanel.SetActive(false);
+            GetComponent<Image>().sprite = hamburgerImg;
+        }
+    }
+}
