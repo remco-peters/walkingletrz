5896b2f84e58ea840e9faad5ecf49331752b8699
Wouter Buytenhuijs <571135@student.inholland.nl>
2019-05-07 15:33:28 +0200

    improvements boosterscene

--- Walking Letrz/Assets/BoosterScene.cs
+++ Walking Letrz/Assets/BoosterScene.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BoosterScene : MonoBehaviour
 {
@@ -13,6 +14,26 @@ public class BoosterScene : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    public void PlayBttnClick(GameObject panel)
+    {
+        Toggle[] toggles = panel.GetComponentsInChildren<Toggle>() ?? new Toggle[] { };
+        foreach(Toggle t in toggles)
+        {
+            if (!t.isOn) continue;
+            Debug.Log(t.GetComponentInChildren<Text>().text);
+        }
+        SceneSwitcher.SwitchSceneStatic("GameScene");
+    }
+
+    public void BoosterBtnClick(Toggle toggle)
+    {
+        ColorBlock cb = toggle.colors;
+        cb.normalColor = toggle.isOn ? new Color(cb.normalColor.r, cb.normalColor.g, cb.normalColor.b, 1) : new Color(cb.normalColor.r, cb.normalColor.g, cb.normalColor.b, 0.5f);
+        cb.highlightedColor = toggle.isOn ? new Color(cb.normalColor.r, cb.normalColor.g, cb.normalColor.b, 1) : new Color(cb.normalColor.r, cb.normalColor.g, cb.normalColor.b, 0.5f);
+        toggle.colors = cb;
         
     }
 }
