d04e7e6c94d7a2e754f0c472d757c74766dd027c
Susanne <sjmprick@gmail.com>
2019-05-14 22:45:28 +0200

    Layout of shop

---
+++ Walking Letrz/Assets/Scripts/ShopButtonHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopButtonHandler : MonoBehaviour
+{
+    public GameObject ShopPanel;
+
+    public void OpenShop()
+    {
+        ShopPanel.SetActive(true);
+    }
+
+    public void CloseShop()
+    {
+        ShopPanel.SetActive(false);
+    }
+}
