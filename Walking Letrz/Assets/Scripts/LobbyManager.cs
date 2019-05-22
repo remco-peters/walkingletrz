40a0148f398fa56eff3c1d314f9c33a025e6e9de
Susanne <sjmprick@gmail.com>
2019-05-22 13:05:22 +0200

    Update lobbyscene

--- Walking Letrz/Assets/Scripts/LobbyManager.cs
+++ Walking Letrz/Assets/Scripts/LobbyManager.cs
@@ -9,7 +9,7 @@ public class LobbyManager : MonoBehaviour
 {
 
     public GameObject usernamePlaceHolder;
-    public Text UsernameClass;
+    public GameObject UsernameClass;
 
     // Start is called before the first frame update
     void Start()
@@ -28,9 +28,7 @@ public class LobbyManager : MonoBehaviour
         Debug.Log(
             $"Player joined: local: {player.IsLocal}, name: {player.NickName}, actornumber: {player.ActorNumber}");
 
-        Text txt = Instantiate(UsernameClass);
-        txt.text = player.NickName;
-        txt.transform.SetParent(usernamePlaceHolder.transform, false);
+        CreatePlayerNameText(player);
     }
     
     void ShowAllPlayers()
@@ -38,9 +36,14 @@ public class LobbyManager : MonoBehaviour
         var list = PhotonManager.PhotonInstance.GetOtherPlayersList();
         foreach (var player in list)
         {
-            Text txt = Instantiate(UsernameClass);
-            txt.text = player.NickName;
-            txt.transform.SetParent(usernamePlaceHolder.transform, false);
+            CreatePlayerNameText(player);
         }
+    }
+
+    void CreatePlayerNameText(Player player)
+    {
+        GameObject txtHolder = Instantiate(UsernameClass);
+        txtHolder.GetComponentInChildren<Text>().text = player.NickName;
+        txtHolder.transform.SetParent(usernamePlaceHolder.transform, false);
     }
 }
