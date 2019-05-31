c56f35aa91e4c049f0e74e014f36c47e2b03f0cb
Remco Peters <remco_peters@outlook.com>
2019-05-31 11:06:35 +0200

    username max 13 characters plus warning in main menu screen

--- Walking Letrz/Assets/Scripts/DisplayNamePopup.cs
+++ Walking Letrz/Assets/Scripts/DisplayNamePopup.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Net.Mime;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class DisplayNamePopup : MonoBehaviour
 {
     public GameObject PopupOverlay;
     public UnityAction<string> OnDisplayNameSave;
+    public Text ErrorText;
 
     private string _displayName;
+    
     // Start is called before the first frame update
     public void OnDisplayNameSaveTouched()
     {
-        OnDisplayNameSave(_displayName);
+        if (_displayName.Length > 13)
+            ErrorText.enabled = true;
+        else
+        {
+            ErrorText.enabled = false;
+            OnDisplayNameSave(_displayName);
+        }
     }
 
     public void OnDisplayNameEditingEnded(string displayName)
