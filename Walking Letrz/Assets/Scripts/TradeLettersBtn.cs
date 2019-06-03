f37718f0333c362be5492b24cea08420cd48b15b
Remco Peters <remco_peters@outlook.com>
2019-06-03 10:29:24 +0200

    trade letters button localized and changes text depending on times traded. when no trades are left the button is set to half opacity

--- Walking Letrz/Assets/Scripts/TradeLettersBtn.cs
+++ Walking Letrz/Assets/Scripts/TradeLettersBtn.cs
@@ -1,8 +1,11 @@
 ﻿using Assets.Scripts;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class TradeLettersBtn : MyMonoBehaviour
 {
+    public Text ButtonText;
+    public Image TradeRotateImage;
     public event UnityAction OnTradeTouched;
     public LetterManager LetterManager { get; set; }
     public void ButtonClicked()
