1ef673dbd928ccbea543377d9b643cc11ac604d6
Remco Peters <remco_peters@outlook.com>
2019-06-05 14:16:20 +0200

    easy medium hard leaderboards

--- Walking Letrz/Assets/Scripts/LeaderBoardScript.cs
+++ Walking Letrz/Assets/Scripts/LeaderBoardScript.cs
@@ -2,15 +2,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LeaderBoardScript : MonoBehaviour
 {
     public GameObject ContentHolder;
     public LeaderboardEntry LeaderboardEntry;
+    public Text LeaderboardTitle;
     
     void Start()
     {
-        foreach (PlayerLeaderboardEntry entry in AccountManager.instance?.GetLeaderboard())
+        AccountManager.instance.OnLeaderboardReceived = (entries) =>
+        {
+            ClearContentHolder();
+            SetLeaderboardEntries(entries);
+        };
+        GetEasyLeaderboard();
+    }
+
+    private void SetLeaderboardTitle(string difficulty)
+    {
+        LeaderboardTitle.text = I2.Loc.LocalizationManager.GetTranslation($"difficulty_{difficulty}");
+    }
+
+    private void ClearContentHolder()
+    {
+        foreach (Transform child in ContentHolder.transform)
+            Destroy(child.gameObject);
+    }
+
+    public void GetEasyLeaderboard()
+    {
+        AccountManager.instance.GetLeaderboard("easy");
+        SetLeaderboardTitle("easy");
+    }
+
+    public void GetMediumLeaderboard()
+    {
+        AccountManager.instance.GetLeaderboard("medium");
+        SetLeaderboardTitle("medium");
+    }
+
+    public void GetHardLeaderboard()
+    {
+        AccountManager.instance.GetLeaderboard("hard");
+        SetLeaderboardTitle("hard");
+    }
+    
+    private void SetLeaderboardEntries(List<PlayerLeaderboardEntry> entries)
+    {
+
+        foreach (PlayerLeaderboardEntry entry in entries)
         {
             LeaderboardEntry newEntry = Instantiate(LeaderboardEntry);
             newEntry.SetName(entry.DisplayName);
